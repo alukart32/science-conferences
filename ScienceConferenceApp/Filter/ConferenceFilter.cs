d6ed1c54c7d5ce19691bc72787fb2b26b2277f04
Ivan_Tarasov <alukart32@gmail.com>
2019-10-01 10:54:51 +0300

    Add ConferenceFilter

---
+++ ScienceConferenceApp/Filter/ConferenceFilter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScienceConferenceApp.Filter
+{
+    class ConferenceFilter
+    {
+        public ConferenceFilter()
+        {
+            conference = 1;
+            subject = 1;
+            theme = 1;
+        }
+
+        public int conference { get; set; }
+        public int subject { get; set; }
+        public string theme { get; set; }
+    }
+}
