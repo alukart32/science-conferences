f88a01ab91d3836dd965b1ad7f4b5132a3524dbd
Ivan_Tarasov <alukart32@gmail.com>
2019-10-06 13:21:36 +0300

    Add CUParticipantFormDTO

---
+++ ScienceConferenceApp/CRUD/DTO/Form/CUParticipantFormDTO.cs
@@ -0,0 +1,22 @@
+﻿using ScienceConferenceApp.CRUD.Model.DTO.Form;
+using ScienceConferenceApp.Forms.Crud;
+using ScienceConferenceApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScienceConferenceApp.CRUD.DTO.Form
+{
+    class CUParticipantFormDTO : CUFormDTO<participant>
+    {
+        public CUParticipantFormDTO()
+        {
+        }
+
+        public CUParticipantFormDTO(DbAppContext db, CrudOpr opr, participant c) : base(db, opr, c)
+        {
+        }
+    }
+}
